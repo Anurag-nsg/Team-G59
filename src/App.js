1b3f3d7d8804c416abaf5694f1e0e5002b380384
Anurag <139263850+Anurag-nsg@users.noreply.github.com>
2024-06-18 20:00:41 +0530

    orgin

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-
+// App.js
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import FaceDetectionUI from './FaceDetectionUI';
 
 function App() {
   return (
-    <div className="App">
-      <>Hello</>
-    </div>
+ 
+      <div className="App">
+        <Routes>
+          <Route path='/' element={<FaceDetectionUI />} />
+        </Routes>
+      </div>
+  
   );
 }
 
