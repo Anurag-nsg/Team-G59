d16accf9fe08e914b802b7cb6a59b3c5404a36b0
Abhi-Gx <abhigxtheupm@gmail.com>
2024-06-26 16:17:51 +0530

    right

--- mesh_networking/device_A/device_A.ino
+++ mesh_networking/device_A/device_A.ino
@@ -1,35 +1,49 @@
-#include "painlessMesh.h"
-//#include <TaskScheduler.h>
-#include <Arduino.h>
+#include <painlessMesh.h>
 
-#define MESH_PREFIX     "yourMeshNetwork"
-#define MESH_PASSWORD   "yourMeshPassword"
-#define MESH_PORT       5555
+#define   MESH_PREFIX     "yourMeshNetwork"
+#define   MESH_PASSWORD   "yourMeshPassword"
+#define   MESH_PORT       5555
 
-Scheduler userScheduler;  // To control your personal task
-painlessMesh mesh;
+Scheduler userScheduler; // to control your personal task
+painlessMesh  mesh;
 
-// User stub
-void sendMessage(); // Prototype, forward declaration
+void sendMessage() ; // Prototype so PlatformIO doesn't complain
 
-Task taskSendMessage(2000, TASK_FOREVER, &sendMessage);
+Task taskSendMessage(TASK_SECOND * 1, TASK_FOREVER, &sendMessage);
 
 void sendMessage() {
-  String msg = "Hello from A";
+  String msg = "Hello from device 1!";
   mesh.sendBroadcast(msg);
-  Serial.printf("Sent message: %s\n", msg.c_str());
+  taskSendMessage.setInterval(TASK_SECOND * 10);
 }
 
-void receivedCallback(uint32_t from, String &msg) {
-  Serial.printf("Received message from %u msg=%s\n", from, msg.c_str());
+void receivedCallback( uint32_t from, String &msg ) {
+  Serial.printf("Received from %u msg=%s\n", from, msg.c_str());
+}
+
+void newConnectionCallback(uint32_t nodeId) {
+  Serial.printf("--> startHere: New Connection, nodeId = %u\n", nodeId);
+  Serial.printf("--> startHere: New Connection, %s\n", mesh.subConnectionJson(true).c_str());
+}
+
+void changedConnectionCallback() {
+  Serial.printf("Changed connections\n");
+}
+
+void nodeTimeAdjustedCallback(int32_t offset) {
+  Serial.printf("Adjusted time %u. Offset = %d\n", mesh.getNodeTime(), offset);
 }
 
 void setup() {
   Serial.begin(115200);
 
-  mesh.setDebugMsgTypes(ERROR | STARTUP | CONNECTION);
-  mesh.init(MESH_PREFIX, MESH_PASSWORD, MESH_PORT);
+  mesh.setDebugMsgTypes(ERROR | STARTUP | CONNECTION);  // set before init() so that you can see startup messages
+
+  mesh.init(MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT);
   mesh.onReceive(&receivedCallback);
+  mesh.onNewConnection(&newConnectionCallback);
+  mesh.onChangedConnections(&changedConnectionCallback);
+  mesh.onNodeTimeAdjusted(&nodeTimeAdjustedCallback);
 
   userScheduler.addTask(taskSendMessage);
   taskSendMessage.enable();
@@ -37,5 +51,4 @@ void setup() {
 
 void loop() {
   mesh.update();
-  userScheduler.execute();
 }
